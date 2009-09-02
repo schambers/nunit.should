7e3cfcdc7d97a1a06f3286170b79d594c2460397
unknown <Administrator@.(none)>
2009-09-02 09:24:05 -0400

    added first set of assertions

---
+++ src/NUnit.Should.Tests/ShouldTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NUnit.Should.Tests
+{
+	[TestFixture]
+	public class ShouldTests
+	{
+		[Test]
+		public void ShouldContainTest()
+		{
+			IList<string> strings = new List<string> {"test"};
+			strings.ShouldContain("test");
+		}
+
+		[Test]
+		public void ShouldNotContainTest()
+		{
+			IList<string> strings = new List<string> {"test"};
+			strings.ShouldNotContain("test2");
+		}
+
+		[Test]
+		public void ShouldBeTest()
+		{
+			true.ShouldBe(true);
+		}
+
+		[Test]
+		public void ShouldBeTrueTest()
+		{
+			true.ShouldBeTrue();
+		}
+
+		[Test]
+		public void ShouldBeFalseTest()
+		{
+			false.ShouldBeFalse();
+		}
+
+		[Test]
+		public void ShouldNotBeNullTest()
+		{
+			new object().ShouldNotBeNull();
+		}
+
+		[Test]
+		public void ShouldBeNullTest()
+		{
+			object bacon = null;
+			bacon.ShouldBeNull();
+		}
+	}
+}
---
+++ src/NUnit.Should/NUnitExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NUnit.Should
+{
+	public static class NUnitExtensions
+	{
+		public static void ShouldBeTrue(this bool expected)
+		{
+			Assert.IsTrue(expected);
+		}
+
+		public static void ShouldBeFalse(this bool expected)
+		{
+			Assert.IsFalse(expected);
+		}
+
+		public static void ShouldNotBeNull<T>(this T expected)
+		{
+			Assert.IsNotNull(expected);
+		}
+
+		public static void ShouldBeNull<T>(this T expected)
+		{
+			Assert.IsNull(expected);
+		}
+
+		public static void ShouldBeGreaterThan<T>(this T expected, T actual) where T : IComparable
+		{
+			Assert.Greater(expected, actual);
+		}
+
+		public static void ShouldBe<T>(this T expected, T actual)
+		{
+			Assert.AreEqual(expected, actual);
+		}
+
+		public static void ShouldBeSameAs<T>(this T expected, T actual)
+		{
+			Assert.AreSame(expected, actual);
+		}
+
+		public static void ShouldContain<T>(this IEnumerable<T> collection, T item)
+		{
+			Assert.Contains(item, collection.ToList());
+		}
+
+		public static void ShouldNotContain<T>(this IEnumerable<T> collection, T item)
+		{
+			Assert.IsFalse(collection.Contains(item));
+		}
+
+		public static void ShouldBeOfType<T>(this object expected)
+		{
+			Assert.IsTrue(expected.GetType() == typeof(T));
+		}
+	}
+}
